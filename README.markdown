424f82e15764af1cfa1bd82d902bba03231c154a
luxansiddharth <92741790+luxansiddharth@users.noreply.github.com>
2022-04-09 11:25:20 +0530

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Save Trees
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/luxansiddharth/pen/zYzPBMg](https://codepen.io/luxansiddharth/pen/zYzPBMg).
+
+
---
+++ script.js
@@ -0,0 +1,4 @@
+function flipimage(){
+  document.getElementById("image1").
+  src="https://images.unsplash.com/photo-1520682464353-63a64fa8bf98?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzMTcxNTc3NA&ixlib=rb-1.2.1&q=80&w=400";
+}
